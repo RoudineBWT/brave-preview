0dc1a6714bf7dcf48b43ee7a2f74214c2c6f4ca0
RoudineBWT <250025757+RoudineBWT@users.noreply.github.com>
2026-06-15 12:32:32 +0000

    chore: update brave versions

--- pkgs/brave-beta.nix
+++ pkgs/brave-beta.nix
@@ -1,7 +1,7 @@
 { callPackage, fetchurl }:
 let
-  version = "1.92.121";
-  hash = "1nd8cs72jjl7mggjsglk53c7xzkzi9r9lw8h2d3rki4xy6yjh8zw";
+  version = "1.92.122";
+  hash = "1h5gf70bdin6wwzdhdcrcmsdd41d432r44b5b9z5qzaban9b4jbh";
 in
 callPackage ./build-brave.nix { } {
   pname = "brave-beta";
--- pkgs/brave-origin-beta.nix
+++ pkgs/brave-origin-beta.nix
@@ -1,7 +1,7 @@
 { callPackage, fetchurl }:
 let
-  version = "1.92.121";
-  hash = "0zh8vyxyk85k7jg5qa89m85bgbx75ikf2jlxpdsil01wklfrasi0";
+  version = "1.92.122";
+  hash = "08kq393vqd1izpn9079c84w489vdcc0fsmyzhbfgljd5w4d1x2b7";
 in
 callPackage ./build-brave.nix { } {
   pname = "brave-origin-beta";
