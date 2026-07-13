d2c227180eb32b5c63480e77107f8505faa4d95f
RoudineBWT <250025757+RoudineBWT@users.noreply.github.com>
2026-07-13 09:40:43 +0000

    chore: update brave versions

--- pkgs/brave-origin-beta.nix
+++ pkgs/brave-origin-beta.nix
@@ -1,7 +1,7 @@
 { callPackage, fetchurl }:
 let
-  version = "1.93.116";
-  hash = "1dkdkc5zmklcjap9h80f0f59ff7i1gfxir4gqgk8qmadjjk67hnq";
+  version = "1.93.117";
+  hash = "09z041magl07jcsbjb9njvv235h042gk8vr15i11mnvsar0dyk8n";
 in
 callPackage ./build-brave.nix { } {
   pname = "brave-origin-beta";
